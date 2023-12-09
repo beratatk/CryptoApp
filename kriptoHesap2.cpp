898b6807c938f9a8a05979c0c88a0f44f2db9df8
Belfort <beratatak@Berat-MacBook-Air.local>
2023-12-09 03:18:54 +0300

    Adding other subjects

--- kriptoHesap2.cpp
+++ kriptoHesap2.cpp
@@ -3,8 +3,9 @@ using namespace std;
 
 class Hesap{
     public:
-        int hesapKriptoBakiye,hesapBakiye;
-        string hesapId;
+        float hesapKriptoBakiye; //hesapKriptoBakiye hesabin kripto bakiyesi
+        float hesapBakiye; //hesapBakiye = hesabin tl bakiyesi
+        string hesapId,hesapTur;
 
         void kriptoParaAl(int kriptoParaAlmaMiktari){
             hesapKriptoBakiye +=kriptoParaAlmaMiktari;
@@ -26,26 +27,32 @@ class Hesap{
 };
 
 int main(){
-    int kriptoTlEder = 1; // 1kripto = 1tl
-    int kullanici_girdi,kullanici_yatirim_miktar;
+    float kriptoGenelTlEder = 1; // 1kripto = 1tl
+    float kriptoOzelTlEder = 1;
+    int kullanici_girdi;
+    float kullanici_yatirim_miktar;
     string kullanici_hesap_no;
     
     int gun=1,ay=1,yil=2000;
-
+    int gun2,ay2,yil2;
     
-    Hesap hesapList[100];
+    Hesap hesapList[1000];//hesap olusturma sinirimiz 1000 adettir burdan degistirebiliriz.
     int i = 0;
     while (true)
 {   
     
     cout<<endl;
-    cout<<"1-Genel Hesap aç\n";
-    cout<<"2-Özel Hesap aç\n";
+    cout<<"1-Genel Hesap aç\n";//gunluk %1 artis, 15 gunde bir %5 dusus
+    cout<<"2-Özel Hesap aç\n";//gunluk %5 artis, 1 ayda rastgele 3 gunde %10 dusus
     cout<<"3-Hesaba para ekle\n";
     cout<<"4-Hesaptan para çek\n";
     cout<<"5-Tarihi göster / düzenle\n";
     cout<<"6-Açılan tüm hesapları listele\n";
-    cout<<"7-Uygulamayı sonlandır.\n";
+    cout<<"7-Kripto para satin al\n";
+    cout<<"8-Kripto para sat\n";
+    cout<<"9-Hesap bakiye sorgula\n";
+    cout<<"10-Hesap karini goster\n";
+    cout<<"11-Uygulamayı sonlandır.\n";
     cout<<"-------------------------------------"<<endl;
     cin >> kullanici_girdi;
 
@@ -55,10 +62,12 @@ int main(){
         cout<<"Genel hesap açmayı seçtiniz...\n";
         cout<<"Hesap no giriniz:";  
         cin>>kullanici_hesap_no;
-        cout<<"Yatıracağınız miktari giriniz:";
+        cout<<"Yatıracağınız miktari giriniz(TL):";
         cin>>kullanici_yatirim_miktar;
         hesapList[i].hesapId = kullanici_hesap_no;
         hesapList[i].hesapBakiye=kullanici_yatirim_miktar;
+        hesapList[i].hesapTur="Genel hesap";
+
         i=i+1;
     }
 
@@ -67,17 +76,18 @@ int main(){
         cout<<"Ozel hesap açmayı seçtiniz...\n";
         cout<<"Hesap no giriniz:";  
         cin>>kullanici_hesap_no;
-        cout<<"Yatıracağınız miktari giriniz:";
+        cout<<"Yatıracağınız miktari giriniz(TL):";
         cin>>kullanici_yatirim_miktar;
         hesapList[i].hesapId = kullanici_hesap_no;
         hesapList[i].hesapBakiye=kullanici_yatirim_miktar;
+        hesapList[i].hesapTur="Ozel hesap";
         i=i+1;
     }
     
     else if (kullanici_girdi==3)
     {   
        int hesapParaEklemeSecim;
-       int hesapParaEklemeMiktar;
+       float hesapParaEklemeMiktar;
        cout<<"Hesaba para ekleme islemini sectiniz...\n";
         
         for (int z = 0; z < i; z++)
@@ -86,25 +96,86 @@ int main(){
         }
        cout<<"Hesap seciniz...";
        cin>>hesapParaEklemeSecim;
-       cout<<"Eklenecek tutari girin:";
+       cout<<"Eklenecek tutari girin(TL):";
        cin>>hesapParaEklemeMiktar;
        hesapList[hesapParaEklemeSecim].hesapBakiye+=hesapParaEklemeMiktar;
+       cout<<"Isleminiz basarili bir sekilde gerceklestirilmistir";
+    }
+    
+    else if (kullanici_girdi==4)
+    {
+       int hesapParaCekmeSecim;
+       float hesapParaCekmeMiktar;
+       cout<<"Hesaptan para cekme islemini sectiniz...\n";
+        
+        for (int z = 0; z < i; z++)
+        {   
+            cout<<z<<"-Hesap ID:"<<hesapList[z].hesapId<<" Bakiye:"<<hesapList[z].hesapBakiye<<endl;
+        }
+       cout<<"Hesap seciniz...";
+       cin>>hesapParaCekmeSecim;
+       cout<<"Cekilecek tutari girin(TL):";
+       cin>>hesapParaCekmeMiktar;
+       if (hesapParaCekmeMiktar>hesapList[hesapParaCekmeSecim].hesapBakiye)
+       {
+        cout<<"Yetersiz bakiye!...\n";
+       }
+       else
+       hesapList[hesapParaCekmeSecim].hesapBakiye-=hesapParaCekmeMiktar;
+       cout<<"Isleminiz basarili bir sekilde gerceklestirilmistir";
+       
     }
     
 
     else if (kullanici_girdi==5)
-    {
+    {   
+        int totalGun=0;
         cout<<"Sistem tarihi:"<<gun<<"."<<ay<<"."<<yil<<endl;
         cout<<"Ana menü için herhangi bir tuşu , tarihi değiştirmek için 0'ı tuşlayın...."<<endl;
         cin>>kullanici_girdi;
         if (kullanici_girdi==0)
         {
            cout<<"Tarih değiştirmeyi seçtiniz, yeni tarihi giriniz...\nGün:";
-           cin>>gun;
+           cin>>gun2;
            cout<<"Ay:";
-           cin>>ay;
+           cin>>ay2;
            cout<<"Yıl:";
-           cin>>yil;
+           cin>>yil2;
+        
+           totalGun = abs(gun2-gun)+abs((ay2-ay)*30)+abs((yil2-yil)*365);
+           gun = gun2;
+           ay = ay2;
+           yil = yil2;
+
+           for (int i = 0; i < totalGun ; i++)
+           {
+            kriptoGenelTlEder += kriptoGenelTlEder*1/100;
+            kriptoOzelTlEder += kriptoOzelTlEder*5/100;
+           }
+           for (int i = 0; i < totalGun/15; i++)
+           {
+            kriptoGenelTlEder -= kriptoGenelTlEder*5/100;
+           }
+           for (int i = 0; i < totalGun/30; i++)//rastgele gunler seciliyor fakat hesaplama sirali oldugu icin hep ayni sonuc geliyor ozel hesap birlikte hesaplanmali ayni dongu icinde
+           {                                    //her ay ayni rastgele gun oluyor, projede rastgele gunlerinde degismesi istenmis!
+            srand(time(NULL));
+            int randomGun1 = rand()%30+1;
+            int randomGun2 = rand()%30+1;
+            int randomGun3 = rand()%30+1; //1 ile 30 arasi bir sayi tut
+            cout<<"Random sayilar:"<<randomGun1<<" "<<randomGun2<<" "<<randomGun3;
+            for (int i = 0; i < 30; i++)
+            {
+                if (i == randomGun1||randomGun2||randomGun3)
+                {
+                    kriptoOzelTlEder -= kriptoOzelTlEder*10/100;
+                }
+            }
+            
+           
+            
+           }
+           cout<<kriptoGenelTlEder<<endl<<kriptoOzelTlEder;
+           
         }
         
     }
@@ -115,12 +186,12 @@ int main(){
         for (int z = 0; z < i; z++)
         {   
             
-            cout<<z<<"-Hesap ID:"<<hesapList[z].hesapId<<" Bakiye:"<<hesapList[z].hesapBakiye<<endl;
+            cout<<z<<"-Hesap ID:"<<hesapList[z].hesapId<<" / Hesap turu:"<<hesapList[z].hesapTur<<" / Bakiye:"<<hesapList[z].hesapBakiye<<endl;
         }
         
     }
     
-    else if (kullanici_girdi==7)
+    else if (kullanici_girdi==11)
     {
         break;
     }
